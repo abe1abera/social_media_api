1ae6ca7f8bde3314ee3c91e663d2967315f695f8
abe1abera <abelaberab@gmail.com>
2025-10-05 23:05:23 +0300

    Add Post and Follow models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    media_url = models.URLField()   
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
